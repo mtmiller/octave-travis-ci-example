ab1b0e59c744b04a54e1eebe5de6970658439e54
Mike Miller <mtmiller@octave.org>
2019-03-24 21:28:59 -0700

    Initial commit

---
+++ hello.m
@@ -0,0 +1,11 @@
+## Copyright (C) 2019 Mike Miller
+## SPDX-License-Identifier: BSD-3-Clause
+
+## -*- texinfo -*-
+## @deftypefn {} {} hello
+## Print a short message to the world.
+## @end deftypefn
+
+function hello ()
+  disp ("Hello, world!")
+endfunction
